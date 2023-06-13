44e60a93da7a89b797f19fabfdc78e7b85c44ed5
windows <windows@microsoft.com>
2023-06-13 18:07:04 +0530

    change

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,35 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {useForm} from 'react-hook-form';
 
 function Form() {
-const [first,second]=useState(1000)
-const [one,b]=useState(0)
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-const abc = (p)=>{
-  b(p.target.value);
-  
-
-}
-
-const myinput = ()=>
-{
-  second(80);
-}
-
+  const myform = data => console.log(data);
 
 
 
   return (
-    <div>
-      <h1>Form</h1>
-    <input type='text' onInput={myinput} value={first}/>
-    <h1>{first}</h1>
+    <form onSubmit={handleSubmit(myform)}>
+    <div className='container bg-light p-5 shadow border' style={{ marginTop: '100px' }} >
+      <div className='row'>
+        <div className='col-md-12 text-center'>
+          <h2 className='border-bottom pb-3'>Registor Form</h2>
+        </div>
+        <div className='col-md-6'>
+          <label class="form-label">Full Name</label>
+          <input type="text" class="form-control" {...register("fullname",{ required: true })} />
+          <p>{errors.fullname && <span className='text-danger'>This field is required</span>}</p>
+        </div>
+        <div className='col-md-6'>
+          <label class="form-label">Email address</label>
+          <input type="email" class="form-control" {...register("email",{ required: true })}/>
+          {errors.email && <span className='text-danger mt-2'>This field is required</span>}
+        </div>
+        <div className='col-md-6'>
+          <label class="form-label">Phone no</label>
+          <input type="text" class="form-control" {...register("phone",{ required: true })}/>
+          {errors.phone && <span className='text-danger mt-2'>This field is required</span>}
+        </div>
+        <div className='col-md-6'>
+          <label class="form-label">Address</label>
+          <input type="text" class="form-control" {...register("address",{ required: true })}/>
+          {errors.address && <span className='text-danger mt-2'>This field is required</span>}
+        </div>
+        <div className='col-md-12 mt-3 pt-2 text-center'>
+          <input type='submit' value="submit" className='btn btn-success'/>
+        </div>
 
 
-<input type='text' value={one} onInput={abc}/>
-<h2>{one}</h2>
+      </div>
 
 
     </div>
+    </form>
   )
 }
 
